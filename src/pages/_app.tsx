24eeeaf1be96a7fe24516cbf631e0e58f85fefa2
Semilore Idowu <semijacks27@gmail.com>
2021-07-12 13:08:10 +0100

    :sparkles: initial commit & register screen

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { Provider, createClient } from "urql";
+
+import theme from "../theme";
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
+
+function MyApp({ Component, pageProps }: any) {
+  return (
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/register.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" label="Username" />
+            <Box mt={4}>
+              <InputField name="password" label="Password" type="password" />
+            </Box>
+            <Button
+              mt={4}
+              colorScheme="teal"
+              type="submit"
+              isLoading={isSubmitting}
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
